6b8baaa47813008c002f39d6363f82c3cabe2d6f
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2022-03-07 19:38:53 -0300

    Feat: Req. 5, 6, 7, 8

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function Login() {
+function Login(props) {
+  const [disable, setDisable] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const btnValidation = () => {
+    const MIN = 6;
+    const valid = email.includes('@')
+    && email.includes('.com')
+    && password.length > (MIN);
+
+    if (valid) {
+      setDisable(false);
+    } else {
+      setDisable(true);
+    }
+  };
+
+  useEffect(() => {
+    btnValidation();
+  });
+
+  const handleSubmit = () => {
+    const { history } = props;
+    history.push('/foods');
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
   return (
     <div>
       <form>
@@ -11,6 +41,7 @@ function Login() {
             placeholder="Email"
             id="email-input"
             autoComplete="off"
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </label>
         <label htmlFor="password-input">
@@ -19,14 +50,24 @@ function Login() {
             type="password"
             placeholder="Password"
             id="password-input"
+            onChange={ (e) => setPassword(e.target.value) }
           />
         </label>
-        <button type="submit" data-testid="login-submit-btn">
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ disable }
+          onClick={ handleSubmit }
+        >
           Enter
         </button>
       </form>
     </div>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default Login;
