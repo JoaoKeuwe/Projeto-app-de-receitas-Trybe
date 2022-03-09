18554abdbd6bc19c3487cd3bbcc007e5dd249fef
Willian <willian.alves.b15@gmail.com>
2022-03-09 20:45:38 -0300

    feat: req-25 and 26

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,26 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { drinksApiMonunt } from '../Services/drinksApi';
 import Context from '../Context/context';
 import Header from '../Components/Header';
 
 function Drinks() {
   const { recipes } = useContext(Context);
+  const [drinksMount, setDrinksMount] = useState();
   const [redirectId, setRedirectId] = useState(false);
+
+  function handleDrinks() {
+    drinksApiMonunt().then((data) => setDrinksMount(data.drinks));
+  }
+
   function handleRedirect() {
     if (recipes && recipes.length === 1) {
       return setRedirectId(true);
     }
   }
   const TWELVE = 12;
-
+  useEffect(() => {
+    handleDrinks();
+  }, []);
   useEffect(() => {
     handleRedirect();
     if (recipes === null) {
       global.alert('Sorry, we haven\'t found any recipes for these filters.');
     }
   });
-
-  console.log(recipes);
   return (
     <div>
       <Header title="Drinks" />
@@ -37,6 +44,21 @@ function Drinks() {
           </p>
         </div>
       ))}
+      { recipes === ''
+        && drinksMount
+        && drinksMount.slice(0, TWELVE).map((drink, index) => (
+          <div width="100px" key={ drink.idDrink } data-testid={ `${index}-recipe-card` }>
+            <img
+              width="100px"
+              src={ drink.strDrinkThumb }
+              alt={ drink.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { drink.strDrink }
+            </p>
+          </div>
+        ))}
       { redirectId && <Redirect to={ `/drinks/${recipes[0].idDrink}` } /> }
     </div>
   );
--- src/Services/drinksApi.js
+++ src/Services/drinksApi.js
@@ -1,4 +1,5 @@
-async function drinksApi(search, radio) {
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export async function drinksApi(search, radio) {
   const URL_INGREDIENT = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${search}`;
   const URL_DRINK = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
   const URL_FIRST_LETTER = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${search}`;
@@ -23,4 +24,8 @@ async function drinksApi(search, radio) {
   }
 }
 
-export default drinksApi;
+export async function drinksApiMonunt() {
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
