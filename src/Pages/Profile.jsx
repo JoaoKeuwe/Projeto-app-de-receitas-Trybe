a4faa14f038688e93a26ae9bde03c55aad8a5a19
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-03-11 16:32:04 -0300

    FIX: tela de perfil

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../Components/Footer';
+import profileIcon from '../images/profileIcon.svg';
+
+function Profile() {
+  const getEmail = localStorage.getItem('user');
+  const test = JSON.parse(getEmail);
+  const { email } = test;
+  const history = useHistory();
+
+  function removeLocalStorage() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('cocktailsToken');
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('inProgressRecipes');
+    history.push('/');
+  }
+
+  function doneRecipes() {
+    history.push('/done-recipes');
+  }
+
+  return (
+    <div>
+      <header>
+        <button type="button" src={ profileIcon }>
+          <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
+        </button>
+        <div>
+          <h1 data-testid="profile-email">{email}</h1>
+
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ doneRecipes }
+          >
+            Done Recipes
+          </button>
+
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/favorite-recipes') }
+          >
+            Favorite Recipes
+          </button>
+
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ removeLocalStorage }
+          >
+            Logout
+          </button>
+
+        </div>
+
+        <h2 data-testid="page-title">Profile</h2>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+export default Profile;
